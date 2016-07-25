8dc09343e4f0bfb52ad31753781e3ff1ae60a83f
Rod Hammond <rodhammond@live.com>
2016-07-25 17:41:49 -0600

    adding Cipher encryption main method

--- src/caesar/Cipher.java
+++ src/caesar/Cipher.java
@@ -1,9 +1,40 @@
 package caesar;
+import java.util.Scanner;
 
-public class Cipher 
-{
+public class Cipher {
 	public static void main(String[] args) 
 	{
+		Scanner keyboard = new Scanner(System.in);
+		String message = null;
+		int mesglength = 0;
+		int shift = 0;
+		
 		System.out.println("Hello World!");
+		System.out.println("Please provide a messasge to encrypt: ");
+		message = keyboard.nextLine().toUpperCase();
+		System.out.println("Please provide shift number: ");
+		shift = keyboard.nextInt();
+		mesglength = message.length();
+		char[] messageC = new char [mesglength];
+		
+		
+		System.out.println("messasge: " + message);
+		for(int i =0; i < mesglength; i++){
+			messageC[i] = message.charAt(i);
+			System.out.println("i : " + i);
+			System.out.println("messasgeC[i]: " + messageC[i]);
+		}
+		
+		for(int i =0; i < mesglength; i++){
+			if ((messageC[i] + shift) > ('Z')){
+				messageC[i] = (char)(shift - 25);
+				System.out.println("New_messasgeC[i]: " + messageC[i]);
+				
+			}
+			else{
+				messageC[i] = (char)(messageC[i] + shift);
+			    System.out.println("New_messasgeC[i]: " + messageC[i]);
+		        }
 	}
+  }
 }

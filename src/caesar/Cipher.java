1d49e9c6b71086487d54542ac2e54a6cceb04490
Rod Hammond <rodhammond@live.com>
2016-07-25 18:22:22 -0600

    adding menu selectiont to main

--- src/caesar/Cipher.java
+++ src/caesar/Cipher.java
@@ -5,36 +5,46 @@ public class Cipher {
 	public static void main(String[] args) 
 	{
 		Scanner keyboard = new Scanner(System.in);
+		char menu;
 		String message = null;
 		int mesglength = 0;
 		int shift = 0;
 		
-		System.out.println("Hello World!");
-		System.out.println("Please provide a messasge to encrypt: ");
-		message = keyboard.nextLine().toUpperCase();
-		System.out.println("Please provide shift number: ");
-		shift = keyboard.nextInt();
-		mesglength = message.length();
-		char[] messageC = new char [mesglength];
+		while(true){
+			System.out.println("Would you like to [E]ncrypt");
+			System.out.println("Would you like to [D]encrypt");
+			System.out.println("Would you like to [Q]uit");
+			menu = keyboard.next().toUpperCase().charAt(0);
+			
+			System.out.println("Please provide a messasge: ");
+			message = keyboard.nextLine().toUpperCase();
+			System.out.println("Please provide shift number: ");
+			shift = keyboard.nextInt();
+			mesglength = message.length();
+			char[] messageC = new char [mesglength];
+			System.out.println("messasge: " + message);
+			for(int i =0; i < mesglength; i++){
+				messageC[i] = message.charAt(i);
+				System.out.println("i : " + i);
+				System.out.println("messasgeC[i]: " + messageC[i]);}
+
+			switch (menu){
+			case 'E':
+				encrypt(shift, messageC);
+				break;
+			case 'D':
+				decrypt(shift, messageC)
+				break;
+			case 'Q':
+				keyboard.close();
+				System.exit(0);
+		    }
 		
-		
-		System.out.println("messasge: " + message);
-		for(int i =0; i < mesglength; i++){
-			messageC[i] = message.charAt(i);
-			System.out.println("i : " + i);
-			System.out.println("messasgeC[i]: " + messageC[i]);
-		}
-		
-		for(int i =0; i < mesglength; i++){
-			if ((messageC[i] + shift) > ('Z')){
-				messageC[i] = (char)('Z' - messageC[i] + 'A');
-				System.out.println("New_messasgeC[i]: " + messageC[i]);
-				
-			}
-			else{
-				messageC[i] = (char)(messageC[i] + shift);
-			    System.out.println("New_messasgeC[i]: " + messageC[i]);
-		        }
 	}
   }
+   // decrypt {}
+	
+   // encrypt {}
+	
+  
 }
